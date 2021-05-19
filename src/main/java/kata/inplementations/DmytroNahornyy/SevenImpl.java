ae057cf1a83b713124bb26035569c74ddca34aa7
dnagor <dnagor0@gmail.com>
2021-05-19 14:33:41 +0300

    Eight

---
+++ src/main/java/kata/inplementations/DmytroNahornyy/SevenImpl.java
@@ -0,0 +1,29 @@
+package kata.inplementations.DmytroNahornyy;
+
+import kata.Seven;
+
+public class SevenImpl implements Seven{
+
+	@Override
+	public long newAvg(double[] arr, double navg) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String seriesSum(int n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int whereIsHe(int p, int bef, int aft) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+	
+	
+	
+}
