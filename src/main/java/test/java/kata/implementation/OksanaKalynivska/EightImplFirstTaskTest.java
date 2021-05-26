65695972436b57fbad700dc7df044076dd90744c
Oksana Kalynivska <o.kalynivska@gmail.com>
2021-05-26 14:12:36 +0300

    some files

--- src/main/java/test/java/kata/implementation/OksanaKalynivska/EightImplFirstTaskTest.java
+++ src/main/java/test/java/kata/implementation/OksanaKalynivska/EightImplFirstTaskTest.java
@@ -5,14 +5,25 @@ import org.testng.annotations.Test;
 
 import kata.implementations.OksanaKalynivska.EightImp;
 
-
 public class EightImplFirstTaskTest {
 	EightImp eight = new EightImp();
 
 	@Test
-	public void testKeepHydrated() {
-		double expResult = eight.liters(2);
-		Assert.assertEquals(1, expResult);
-		
+	public void test_1_KeepHydrated() {
+		double actualResult = eight.liters(2);
+		Assert.assertEquals(1, actualResult);
+
+	}
+
+	@Test
+	public void test_2_KeepHydrated() {
+		double actualResult = eight.liters(0.97);
+		Assert.assertEquals(0, actualResult);
+	}
+
+	@Test
+	public void test_3_KeepHydrated() {
+		double actualResult = eight.liters(1600.20);
+		Assert.assertEquals(800, actualResult);
 	}
 }
