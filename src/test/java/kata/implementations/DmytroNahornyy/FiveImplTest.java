801dce57e65ff4a139f9c39b0837ac64e996585b
dnagor <dnagor0@gmail.com>
2021-05-26 13:24:40 +0300

    adding

--- src/test/java/kata/implementations/DmytroNahornyy/FiveImplTest.java
+++ src/test/java/kata/implementations/DmytroNahornyy/FiveImplTest.java
@@ -1,5 +1,6 @@
 package kata.implementations.DmytroNahornyy;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
@@ -10,15 +11,18 @@ public class FiveImplTest {
     public void testZeros() {
         int expectedResult = 0;
         int actualResult = five.zeros(0);
+        Assert.assertEquals(expectedResult,actualResult);
     }
     @Test
     public void testZeros2() {
         int expectedResult = 1;
         int actualResult = five.zeros(6);
+        Assert.assertEquals(expectedResult,actualResult);
     }
     @Test
     public void testZeros3() {
         int expectedResult = 2;
         int actualResult = five.zeros(14);
+        Assert.assertEquals(expectedResult,actualResult);
     }
 }
