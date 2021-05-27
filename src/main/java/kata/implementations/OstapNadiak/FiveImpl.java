e8a1cc52532a9419af5de6642427a6d0583c3506
ONadiak <nadiak200202@gmail.com>
2021-05-27 12:38:14 +0300

    Added 2 tasks

--- src/main/java/kata/implementations/OstapNadiak/FiveImpl.java
+++ src/main/java/kata/implementations/OstapNadiak/FiveImpl.java
@@ -3,6 +3,7 @@ package kata.implementations.OstapNadiak;
 import kata.Five;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 
 public class FiveImpl implements Five {
 
@@ -51,17 +52,78 @@ public class FiveImpl implements Five {
 
     @Override
     public long[] gap(int g, long m, long n) {
-        return new long[0];
+        long[] primes = new long[2];
+        BigInteger integer = new BigInteger(String.valueOf(m));
+        boolean probablePrime;
+        ArrayList<Integer> primeNumbers = new ArrayList<>();
+        for (int i ; m<n;m++){
+            integer = BigInteger.valueOf(m);
+            probablePrime = integer.isProbablePrime((int)m);
+            if ( probablePrime ){
+                primeNumbers.add((int)m);
+            }
+        }
+        for (int i=0; i<(primeNumbers.size()-1); i++){
+            if (primeNumbers.get(i+1) - primeNumbers.get(i) == g){
+                primes[0] = primeNumbers.get(i);
+                primes[1] = primeNumbers.get(i+1);
+                break;
+            }
+        }
+        return primes;
+    }
+    public static int getFactorial(int f) {
+        int result = 1;
+        for (int i = 1; i <= f; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+
+    public static int getCountsOfDigits(long number) {
+        return String.valueOf(Math.abs(number)).length();
+    }
+
+    static int[] fib(int n)
+    {
+
+        int f[] = new int[n+2];
+        int i;
+        f[0] = 0;
+        f[1] = 1;
+        for (i = 2; i <= n+1; i++)
+        {
+            f[i] = f[i-1] + f[i-2];
+        }
+        return f;
     }
 
     @Override
     public int zeros(int n) {
-        return 0;
+        int count=0;
+        long number;
+        number = getFactorial(n);
+        for (int i = 0; i<getCountsOfDigits(number); i++){
+            if (number%10 == 0){
+                count++;
+                number = number/10;
+            }
+            else {
+                break;
+            }
+        }
+        return count;
     }
 
     @Override
     public BigInteger perimeter(BigInteger n) {
-        return null;
+        int[] fibonaciNumbers =  fib(n.intValue());
+        int sumOfPerimetres = 0;
+        for (int i =0; i< (n.intValue()+2);i++){
+            sumOfPerimetres += fibonaciNumbers[i]*4;
+        }
+        n = BigInteger.valueOf(sumOfPerimetres);
+        return n;
     }
 
     @Override
