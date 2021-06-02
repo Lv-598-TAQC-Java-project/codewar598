4444797f9801771becdee80e8e3e864aa62f178f
AlonaLutsenko <alonalutsenkobb@gmail.com>
2021-06-02 12:21:19 +0300

    test change

--- src/test/java/kata/six/StockSummaryTest.java
+++ src/test/java/kata/six/StockSummaryTest.java
@@ -12,14 +12,9 @@ public class StockSummaryTest {
     @Test(dataProvider = "impl6", dataProviderClass = DataProvider.class)
     public void testStockSummary(Six six) {
         System.out.println(String.format("TestPositive checking for implementation: %s", six));
-        System.out.println(String.format("TestPositive checking for implementation: %s", six));
 
         String[] art = new String[]{"ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"};
         String[] cd = new String[]{"A", "B"};
-        try {
-            Assert.assertTrue("(A : 200) - (B : 1140)".equals(six.stockSummary(art, cd)));
-        } catch (AssertionError e) {
-            System.out.println("Negative realization");
-        }
+
     }
 }
