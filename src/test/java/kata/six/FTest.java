633dc7d2eabd3dc7b3ba1bb2149ada4d6578efa3
Orest Torskyi <Orest.Torskyi.KN.2017@gmail.com>
2021-06-16 15:00:15 +0300

    Added test for Floating Approximation Task

--- src/test/java/kata/six/FTest.java
+++ src/test/java/kata/six/FTest.java
@@ -2,7 +2,10 @@ package kata.six;
 
 import kata.DataProvider;
 import kata.Six;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.text.DecimalFormat;
 
 
 public class FTest  {
@@ -10,7 +13,21 @@ private Six six;
 
     @Test(dataProvider =  "impl6",dataProviderClass = DataProvider.class)
     public void testF(Six six) {
-     //To Do...
+        double expectedData = 2.499996875007812e-06;
+        double actualData = six.f(5.0e-06);
+        boolean isInRange;
+        double limit = 1e-12;
+        if (expectedData == 0.0)
+            isInRange = Math.abs(six.f(5.0e-06)) <= limit;
+        else {
+            double e = Math.abs((actualData - expectedData) / expectedData);
+            isInRange = e <= 1e-12;
+        }
+        if (isInRange == false) {
+            DecimalFormat df = new DecimalFormat("#.0000000000000000");
+            System.out.println("Expected must be near " + expectedData +", but got " + actualData);
+        }
+        Assert.assertEquals(true, isInRange);
     }
 
 }
